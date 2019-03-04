2fc98f08cc5f918bae0ed11336435dc81e4aeafe
alisha101 <alafshafazlani@gmail.com>
2019-03-04 15:27:31 +0530

    base url

--- src/app/base_url.ts
+++ src/app/base_url.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:8085/Spring4AnnotationConfig/";
+//export const BASE_URL = "http://localhost:8085/Spring4AnnotationConfig/";
 // export const BASE_URL = "http://54.185.209.216:8080/Spring4AnnotationConfig/";
-// export const BASE_URL = "http://54.188.124.87:8080/Spring4AnnotationConfig/";
+export const BASE_URL = "http://54.188.124.87:8080/Spring4AnnotationConfig/";
